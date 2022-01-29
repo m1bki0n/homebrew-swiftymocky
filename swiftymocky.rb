0bb0164e8b8962f015c1e31a6e462d44815f4951
Makwan Barzan <makwanbarzan@gmail.com>
2022-01-29 23:40:47 +0300

    Update swiftymocky.rb

--- swiftymocky.rb
+++ swiftymocky.rb
@@ -2,7 +2,7 @@ class Swiftymocky < Formula
   desc "Framework for automatic mock generation. Adds a set of handy methods, simplifying testing. One of the best and most complete solutions, including generics support and much more."
   homepage "https://github.com/MakeAWishFoundation/SwiftyMocky"
   url "https://github.com/MakeAWishFoundation/SwiftyMocky/archive/refs/tags/3.6.0.tar.gz"
-  sha256 "729a95f6ed232ca1f660b6e549649dd1e6b9b4960583434e5de64232b1e1a3e3"
+  sha256 "d71c9300db9c2fa1040ed29470710b577950cb62530d05ac609492c6e8ba5ed8"
   license "MIT"
 
   depends_on xcode: "13.0"
